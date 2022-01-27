a3e4eb2e475cb6b66490304360e04c7bee4df7e9
Jeremie Lucotte <lucottejeremie@gmail.com>
2022-01-27 14:04:41 +0100

    Updates

---
+++ precache-manifest.38ac749262cc096696050038f3366e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a182c59ccc3ed33e75ef55b4a5448732",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/index.html"
+  },
+  {
+    "revision": "8eae527857e66ba45b68",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "44cbf9e4ba1e8fd91688",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8eae527857e66ba45b68",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/static/js/2.731f9151.chunk.js"
+  },
+  {
+    "revision": "44cbf9e4ba1e8fd91688",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/static/js/main.85d254bd.chunk.js"
+  },
+  {
+    "revision": "8cf41f8c97f2e8c251a3",
+    "url": "/Developpeur-Ethereum-Template/6.%20Dapp/defi3/static/js/runtime-main.68f61961.js"
+  }
+]);
